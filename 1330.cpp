141391f4defb203aca175930a317ad2da1480118
pinguvenom <pinguvenom@gmail.com>
2024-03-27 23:08:15 +0900

    Solve 1330, 9498, 2753, 14681.

---
+++ 1330.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+int main() {
+  int a, b;
+  std::cin >> a >> b;
+  if (a > b) {
+    std::cout << ">";
+  } else if (a < b) {
+    std::cout << "<";
+  } else {
+    std::cout << "==";
+  }
+  return 0;
+}
---
+++ 14681.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+int main() {
+  int x, y;
+  std::cin >> x >> y;
+  if (x > 0) {
+    if (y > 0) {
+      std::cout << "1";
+    } else {
+      std::cout << "4";
+    }
+  } else {
+    if (y > 0) {
+      std::cout << "2";
+    } else {
+      std::cout << "3";
+    }
+  }
+  return 0;
+}
