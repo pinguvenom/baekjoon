09b8e1b49f87041cf462d7b8d2a8ca8ae43aa641
pinguvenom <pinguvenom@gmail.com>
2024-03-29 09:06:23 +0900

    Solve 2739, 10950, 8393.

---
+++ 10950.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+int main() {
+  int t;
+  std::cin >> t;
+  for (int i = 0; i < t; i++) {
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << a + b << "\n";
+  }
+
+  return 0;
+}
---
+++ 8393.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+int main() {
+  int n, sum = 0;
+  std::cin >> n;
+
+  for (int i = 1; i <= n; i++) {
+    sum += i;
+  }
+
+  std::cout << sum;
+  return 0;
+}
