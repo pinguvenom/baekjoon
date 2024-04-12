1e8d67abba5c89b1bb42711d8a7bed30c88cd324
pinguvenom <pinguvenom@gmail.com>
2024-04-12 10:18:58 +0900

    Solve 2309 using for loop combination.

---
+++ 2309.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+pair<int, int> solve(int arr[]) {
+  int sum = std::accumulate(arr, arr + 9, 0);
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < i; j++) {
+      if (sum - arr[i] - arr[j] == 100) {
+        return {i, j};
+      }
+    }
+  }
+}
+
+int main() {
+  int arr[9];
+  for (int i = 0; i < 9; i++) {
+    cin >> arr[i];
+  }
+
+  pair<int, int> outlier = solve(arr);
+
+  vector<int> rst;
+  for (int i = 0; i < 9; i++) {
+    if (i == outlier.first || i == outlier.second) {
+      continue;
+    }
+    rst.push_back(arr[i]);
+  }
+
+  sort(rst.begin(), rst.end());
+  for (int i : rst) {
+    std::cout << i << "\n";
+  }
+
+  return 0;
+}
